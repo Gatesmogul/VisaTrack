28458f90aceb67e4427a11d15897b2f5fc74bcf5
Joshua Okoro <blavk014@gmail.com>
2026-01-13 19:12:45 +0100

    desktop update

---
+++ Frontend/JS/submit.js
@@ -0,0 +1,11 @@
+document.getElementById("backBtn").addEventListener("click", () => {
+  window.history.back();
+});
+
+document.getElementById("viewStatusBtn").addEventListener("click", () => {
+  window.location.href = "";
+});
+
+document.getElementById("backHomeBtn").addEventListener("click", () => {
+  window.location.href = "";
+});
